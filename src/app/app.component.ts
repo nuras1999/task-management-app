5ce58ffc6517e03516bebc19b447aae27ce2c7d5
nuras1999 <nuras1999@gmail.com>
2024-07-04 14:35:58 +0530

    feat(sidenav): toggle sidenav and responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { Router, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  constructor(private router: Router) {}
+  /**
+   * Show/hide navigation menu for small devices
+   */
+  public showNavMenu = false;
+
+  constructor(public router: Router) {}
 
   /**
    * Navigate to the specified route
@@ -17,5 +23,12 @@ export class AppComponent {
    */
   public navigateTo(routeName: string): void {
     this.router.navigate([routeName]);
+  }
+
+  /**
+   * Toggle show/hide icon for sidenav
+   */
+  public toggleNavMenu(): void {
+    this.showNavMenu = !this.showNavMenu;
   }
 }
