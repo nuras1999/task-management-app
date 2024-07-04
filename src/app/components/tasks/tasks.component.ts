d6e4e27dc9703d9d6c8fc05dc493c4cff4d8af5b
nuras1999 <nuras1999@gmail.com>
2024-07-04 21:06:56 +0530

    feat(tasks): implement delete functionality

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -12,4 +12,12 @@ export class TasksComponent {
    * List of tasks
    */
   public myTasks = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5"];
+
+  /**
+   * Delete tasks using its index
+   * @param index - Index of tasks to delete
+   */
+  public deleteTask(index: number): void {
+    this.myTasks.splice(index, 1);
+  }
 }
